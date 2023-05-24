9d0079fbcd87ac56844e74a44f8ab1fc7b438238
özgebayram <ozgebayram1234@gmail.com>
2023-05-24 23:28:20 +0300

    feat: create button

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { Button } from "antd";
 function App() {
   return (
     <div>
-      {" "}
+      
       <Button type="primary">Primary Button</Button>
+      <Button type="primary" danger>Primary </Button>
+
       <h1 className="text-3xl font-bold underline text-red-600">Hello world!</h1>
     </div>
   );
