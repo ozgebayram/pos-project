a354f792d1fa0b1b0519c80f3d18d15cd63170c3
özgebayram <ozgebayram1234@gmail.com>
2023-05-26 03:08:58 +0300

    feat: add Header.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { Button } from "antd";
+
+import Header from "./components/header/Header";
+
+
+function App() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,89 @@
+import { Input, Badge } from "antd";
+import {
+  SearchOutlined,
+  HomeOutlined,
+  ShoppingCartOutlined,
+  CopyOutlined,
+  UserOutlined,
+  BarChartOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
+
+const Header = () => {
+  return (
+    <div className="border-b mb-6">
+      <header className="py-4 px-6 flex justify-between items-center gap-10">
+        <div className="logo">
+          <a href="/">
+            <h2 className="text-2xl font-bold md:text-4xl ">LOGO</h2>
+          </a>
+        </div>
+        <div className="header-search flex-1 flex justify-center">
+          <Input
+            size="large"
+            placeholder="Search..."
+            prefix={<SearchOutlined />}
+            className="rounded-full max-w-[800px]"
+          />
+        </div>
+        <div className="menu-links flex justify-between items-center gap-7 md:static fixed bottom-0 md:w-auto w-screen md:bg-transparent bg-white left-0 md:border-t-0 border-t md:px-0 px-4 py-1">
+          <a
+            href={"/"}
+            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
+          >
+            <HomeOutlined className="md:text-2xl text-xl" />
+            <span className="md:text-xs text-[10px]">Home</span>
+          </a>
+          <Badge count={5} offset={[0, 6]} className="md:flex hidden">
+            <a
+              href={"/"}
+              className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
+            >
+              <ShoppingCartOutlined className="md:text-2xl text-xl" />
+              <span className="md:text-xs text-[10px]">Cart</span>
+            </a>
+          </Badge>
+          <a
+            href={"/"}
+            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
+          >
+            <CopyOutlined className="md:text-2xl text-xl" />
+            <span className="md:text-xs text-[10px]">Invoice</span>
+          </a>
+          <a
+            href={"/"}
+            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
+          >
+            <UserOutlined className="md:text-2xl text-xl" />
+            <span className="md:text-xs text-[10px]">Customers</span>
+          </a>
+          <a
+            href={"/"}
+            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
+          >
+            <BarChartOutlined className="md:text-2xl text-xl" />
+            <span className="md:text-xs text-[10px]">Statistics</span>
+          </a>
+          <a
+            href={"/"}
+            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
+          >
+            <LogoutOutlined className="md:text-2xl text-xl" />
+            <span className="md:text-xs text-[10px]">Logout</span>
+          </a>
+        </div>
+		<Badge count={5} offset={[0, 6]} className="md:hidden flex ">
+            <a
+              href={"/"}
+              className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
+            >
+              <ShoppingCartOutlined className=" text-2xl" />
+              <span className="md:text-xs text-[10px]">Cart</span>
+            </a>
+          </Badge>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
