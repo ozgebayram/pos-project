46df8f3b96e94ddc21b546814f5318bf355bd530
özgebayram <ozgebayram1234@gmail.com>
2023-05-28 23:58:51 +0300

    feat: categoiries css

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -2,8 +2,14 @@
 const Categories = () => {
   return (
 	<div>
-		<ul>
-			<li className="bg-green-700 px-6 py-10 text-white ">
+		<ul className="flex gap-4 flex-col">
+			<li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all ">
+				<span>All</span>
+			</li>
+			<li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all ">
+				<span>All</span>
+			</li>
+			<li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all ">
 				<span>All</span>
 			</li>
 		</ul>
