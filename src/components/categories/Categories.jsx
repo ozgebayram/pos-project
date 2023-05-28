ec819bc9591514e25f25cc7365ea2a12285bf5e7
özgebayram <ozgebayram1234@gmail.com>
2023-05-28 23:59:45 +0300

    feat:css changed

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -3,13 +3,13 @@ const Categories = () => {
   return (
 	<div>
 		<ul className="flex gap-4 flex-col">
-			<li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all ">
+			<li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
 				<span>All</span>
 			</li>
-			<li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all ">
+			<li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
 				<span>All</span>
 			</li>
-			<li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all ">
+			<li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
 				<span>All</span>
 			</li>
 		</ul>
