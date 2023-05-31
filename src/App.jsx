8f055d0e47b4afbf29c08b293a355d4146d82691
özgebayram <ozgebayram1234@gmail.com>
2023-05-31 16:32:48 +0300

    feat: add cart totals

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 // import { Button } from "antd";
 // import { Button } from "antd";
 
+import CartTotals from "./components/cart/CartTotals";
 import Categories from "./components/categories/Categories";
 import Header from "./components/header/Header";
+import Products from "./components/products/Products";
 
 
 function App() {
@@ -10,21 +12,23 @@ function App() {
     <>
       <Header/>
       <div className="home px-6 flex justify-between gap-10">
-        <div className="categories flex-1 overflow-auto max-h-[calc(100vh-_-112px)] pb-64">
+        <div className="categories  overflow-auto max-h-[calc(100vh-_-112px)] pb-64">
           {/* yukarıda scroll verdik ve pb-64 ile de scroll un sonunda boşluk bıraktık */}
           {/* bu bir slider kutuphanesi ile d eyapılabılır */}
           <Categories/>
         </div>
         <div className="products flex-[8]">
-          <div>products</div>
+          <Products/>
         </div>
-        <div className="card-totals">
-          <div>card totals</div>
+        <div className="card-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border">
+          <CartTotals/>
         </div>
       </div>
     </>
   );
 }
 
-
+//  
 export default App;
+
+// md:-mr-[24px] md:-mt-[24px] = bu özellik shopping list yazısını yukarı ve sola sıfırladı
--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -2,32 +2,32 @@
 const Categories = () => {
   return (
 	<div>
-		<ul className="flex gap-4 flex-col text-xl">
-			<li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+		<ul className="flex gap-4 flex-col text-lg">
+			<li className=" min-w[145px]">
 				<span>All</span>
 			</li>
-			<li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+			<li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
 				<span>All</span>
 			</li>
-			<li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+			<li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
 				<span>All</span>
 			</li>
-			<li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+			<li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
 				<span>All</span>
 			</li>
-			<li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+			<li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
 				<span>All</span>
 			</li>
-			<li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+			<li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
 				<span>All</span>
 			</li>
-			<li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+			<li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
 				<span>All</span>
 			</li>
-			<li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+			<li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
 				<span>All</span>
 			</li>
-			<li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+			<li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
 				<span>All</span>
 			</li>
 		</ul>
