68f43259ab604909612999facdfe9c3e49ed88e3
özgebayram <ozgebayram1234@gmail.com>
2023-06-30 20:44:11 +0300

    feat: statistic-card

--- src/pages/StatisticPage.jsx
+++ src/pages/StatisticPage.jsx
@@ -1,4 +1,3 @@
-
 import Header from "../components/header/Header";
 
 const StatisticPage = () => {
@@ -7,11 +6,18 @@ const StatisticPage = () => {
       <Header />
       <div className="px-6">
         <h1 className="text-4xl font-bold text-center mb-4">Statistics</h1>
-		<div>
-			<h2 className="text-lg">Welcome <span className="text-green-700 font-bold text-xl">Admin</span> </h2>
-		</div>
+        <div className="statistic-section">
+          <h2 className="text-lg">
+            Welcome{" "}
+            <span className="text-green-700 font-bold text-xl">Admin</span>{" "}
+          </h2>
+		  <div className="statsitic-cards grid grid-cols-4">
+			<div className="card-item bg-gray-800">
+				sdfg
+			</div>
+		  </div>
+        </div>
       </div>
-     
     </>
   );
 };
