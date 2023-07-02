86761146f24504396e89e4965f6875f9617dad18
özgebayram <ozgebayram1234@gmail.com>
2023-07-03 02:25:37 +0300

    feat:statistic cards

--- src/pages/StatisticPage.jsx
+++ src/pages/StatisticPage.jsx
@@ -9,13 +9,21 @@ const StatisticPage = () => {
         <div className="statistic-section">
           <h2 className="text-lg">
             Welcome{" "}
-            <span className="text-green-700 font-bold text-xl">Admin</span>{" "}
+            <span className="text-green-700 font-bold text-xl">Admin</span>
           </h2>
-		  <div className="statistic-cards  grid grid-cols-card">
-			<div className="card-item bg-gray-800">
-				sdfg
-			</div>
-		  </div>
+          <div className="statistic-cards flex my-10">
+            <div className="card-item bg-gray-800 p-8 rounded-lg">
+              <div className="flex gap-x-4">
+                <div className="rounded-full bg-white w-16 h-16 p-3">
+                  <img src="images/user.png" alt="user" />
+                </div>
+                <div className="text-white">
+                  <p className="mb-2 text-lg font-medium text-gray-400">Total Customers</p>
+                  <p className="text-xl font-semibold text-gray-200">6</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
